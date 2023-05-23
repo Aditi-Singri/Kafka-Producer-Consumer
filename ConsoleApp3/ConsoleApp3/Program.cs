52c25baed0df5f2f4e0f673e45e8407119b6708f
Aditi Singri <Aditi@vrize.com>
2023-05-23 11:30:44 +0530

    Adding producer & docker compose

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        //private const string BootstrapServers = "localhost:9092";
+        private const string Topic = "topic2";
+
+
+
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter a message (or 'exit' to quit):");
+                    var message = Console.ReadLine();
+
+                    if (message == "exit")
+                        break;
+
+                    var messageValue = JsonConvert.SerializeObject(message);
+                    var result = await producer.ProduceAsync(Topic, new Message<Null, string> { Value = messageValue });
+
+
+                    Console.WriteLine($"Message delivered to {result.TopicPartitionOffset}");
+                }
+            }
+        }
+    }
+}
